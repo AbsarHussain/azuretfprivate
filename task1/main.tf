4fac8071ecfa0c7717223115f717276c1b8f8ffc
AbsarHussain <34912789+AbsarHussain@users.noreply.github.com>
2021-05-20 21:29:21 +0500

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -4,10 +4,10 @@ provider "azurerm" {
 
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+  name     = "absarrg"
+  location = "East US"
 
   tags = {
-    environment = "production"
+    environment = "salman"
   }
 }
