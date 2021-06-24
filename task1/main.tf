e0fe46bb76ca8481accfcec1aa3c67ea8352caf4
AbsarHussain <34912789+AbsarHussain@users.noreply.github.com>
2021-06-24 23:47:20 +0500

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -1,10 +1,17 @@
 provider "azurerm" {
   features {}
 }
+data "azurerm_key_vault_secret" "example" {
+  name         = "vmname"
+  key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/absarresg/providers/Microsoft.KeyVault/vaults/azurekeyvaultforpiplein"
+}
 
+output "secret_value" {
+  value = data.azurerm_key_vault_secret.example.value
+}
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "rg" {
-  name     = $(vmname)
+  name     = secret_value
   location = var.resource_group_location
 
   tags = {
