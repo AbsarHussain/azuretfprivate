4d316bc1b826d53edb2a51e817e83991f10c6450
AbsarHussain <34912789+AbsarHussain@users.noreply.github.com>
2021-06-25 23:52:14 +0500

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -45,8 +45,8 @@ data "azurerm_key_vault_secret" "networksecuritygroupname" {
  
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "rg" {
-  name     = "howden_rg"
-  location = "East US"
+  name     = data.azurerm_key_vault_secret.resourcegroupname.value
+  location = data.azurerm_key_vault_secret.resourcegrouplocation.value
 
   tags = {
     environment = "salman"
