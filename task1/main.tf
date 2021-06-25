3cee0c1be071d083db8730104325ce73835a1e7d
AbsarHussain <34912789+AbsarHussain@users.noreply.github.com>
2021-06-25 20:20:51 +0500

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -1,24 +1,24 @@
 provider "azurerm" {
   features {}
 }
-data "azurerm_key_vault_secret" "example" {
-  name         = "vmname"
-  key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/absarresg/providers/Microsoft.KeyVault/vaults/azurekeyvaultforpiplein"
-}
+# data "azurerm_key_vault_secret" "example" {
+#   name         = "vmname"
+#   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/absarresg/providers/Microsoft.KeyVault/vaults/azurekeyvaultforpiplein"
+# }
 
-output "secret_value"  {
-  value = data.azurerm_key_vault_secret.example.value
-  sensitive = true
-}
-# Create a resource group if it doesn't exist
-resource "azurerm_resource_group" "rg" {
-  name     = data.azurerm_key_vault_secret.example.value
-  location = "East US"
+# output "secret_value"  {
+#   value = data.azurerm_key_vault_secret.example.value
+#   sensitive = true
+# }
+# # Create a resource group if it doesn't exist
+# resource "azurerm_resource_group" "rg" {
+#   name     = data.azurerm_key_vault_secret.example.value
+#   location = "East US"
 
-  tags = {
-    environment = "production"
-  }
-}
+#   tags = {
+#     environment = "production"
+#   }
+# }
 
 resource "azurerm_resource_group" "rgsecond" {
   name     = "firstresourcegroupfrompipeline"
