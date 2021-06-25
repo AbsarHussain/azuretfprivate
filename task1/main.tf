2bf755b1d51c622fc94da35eefd340a26901502c
AbsarHussain <34912789+AbsarHussain@users.noreply.github.com>
2021-06-25 23:31:14 +0500

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -5,42 +5,42 @@ provider "azurerm" {
 
 data "azurerm_key_vault_secret" "networkinterfacename" {
    name         = "networkinterfacename"
-   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/howden_rg/providers/Microsoft.KeyVault/vaults/howdenrgkeyvault"
+   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/firstresourcegroupfrompipeline/providers/Microsoft.KeyVault/vaults/vmsecretspipeline"
  }
 
 data "azurerm_key_vault_secret" "networksecuritygroupname" {
    name         = "networksecuritygroupname"
-   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/howden_rg/providers/Microsoft.KeyVault/vaults/howdenrgkeyvault"
+   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/firstresourcegroupfrompipeline/providers/Microsoft.KeyVault/vaults/vmsecretspipeline"
  }
  
  data "azurerm_key_vault_secret" "publicipname" {
    name         = "publicipname"
-   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/howden_rg/providers/Microsoft.KeyVault/vaults/howdenrgkeyvault"
+   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/firstresourcegroupfrompipeline/providers/Microsoft.KeyVault/vaults/vmsecretspipeline"
  }
  
  data "azurerm_key_vault_secret" "resourcegrouplocation" {
    name         = "resourcegrouplocation"
-   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/howden_rg/providers/Microsoft.KeyVault/vaults/howdenrgkeyvault"
+   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/firstresourcegroupfrompipeline/providers/Microsoft.KeyVault/vaults/vmsecretspipeline"
  }
  
  data "azurerm_key_vault_secret" "resourcegroupname" {
    name         = "resourcegroupname"
-   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/howden_rg/providers/Microsoft.KeyVault/vaults/howdenrgkeyvault"
+   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/firstresourcegroupfrompipeline/providers/Microsoft.KeyVault/vaults/vmsecretspipeline"
  }
  
  data "azurerm_key_vault_secret" "subnetname" {
    name         = "subnetname"
-   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/howden_rg/providers/Microsoft.KeyVault/vaults/howdenrgkeyvault"
+   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/firstresourcegroupfrompipeline/providers/Microsoft.KeyVault/vaults/vmsecretspipeline"
  }
  
  data "azurerm_key_vault_secret" "virtualnetworkname" {
    name         = "virtualnetworkname"
-   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/howden_rg/providers/Microsoft.KeyVault/vaults/howdenrgkeyvault"
+   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/firstresourcegroupfrompipeline/providers/Microsoft.KeyVault/vaults/vmsecretspipeline"
  }
  
   data "azurerm_key_vault_secret" "linuxvirtualmachinename" {
    name         = "linuxvirtualmachinename"
-   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/howden_rg/providers/Microsoft.KeyVault/vaults/howdenrgkeyvault"
+   key_vault_id = "/subscriptions/23fcce43-2d2f-4071-9e69-fdb25386438a/resourceGroups/firstresourcegroupfrompipeline/providers/Microsoft.KeyVault/vaults/vmsecretspipeline"
  }
  
 # Create a resource group if it doesn't exist
